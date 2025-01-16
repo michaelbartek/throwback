61ce0e6040a9b45a09509652821dee54b3ab3796
michaelbartek <michal.bartek2004@gmail.com>
2025-01-16 07:16:41 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset();  // Odstráni všetky session premenné
-session_destroy(); // Zničí session
-header("Location: index.php");  // Presmeruje späť na login stránku
-exit();
