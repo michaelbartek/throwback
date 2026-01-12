f0336a6634743cdc5bb4fbf86308c580f3a231ea
michaelbartek <michal.bartek2004@gmail.com>
2026-01-12 20:32:27 +0100

    Add files via upload

--- spejs/java/cursor.js
+++ spejs/java/cursor.js
@@ -1,14 +1,21 @@
 const cursor = document.querySelector('.cursor');
 
-document.addEventListener('mousemove', e => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-});
+/* stop if touch device */
+if (window.matchMedia("(hover: none) and (pointer: coarse)").matches) {
+    cursor.remove();
+} else {
 
-document.addEventListener('mousedown', () => {
-    cursor.classList.add('click');
-});
+    document.addEventListener('mousemove', e => {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+    });
 
-document.addEventListener('mouseup', () => {
-    cursor.classList.remove('click');
-});
+    document.addEventListener('mousedown', () => {
+        cursor.classList.add('click');
+    });
+
+    document.addEventListener('mouseup', () => {
+        cursor.classList.remove('click');
+    });
+
+}
