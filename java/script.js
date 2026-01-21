0ac6ad66426bfcf6445d5d610369443e676c6959
michaelbartek <michal.bartek2004@gmail.com>
2026-01-21 23:18:17 +0100

    Update script.js

--- java/script.js
+++ java/script.js
@@ -14,3 +14,35 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 2000);
+<script>
+function loadGoogleAnalytics() {
+  var s = document.createElement('script');
+  s.src = "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX";
+  s.async = true;
+  document.head.appendChild(s);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XXXXXXX');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var consent = localStorage.getItem("cookieConsent");
+  var banner = document.getElementById("cookie-banner");
+
+  if (!consent) banner.style.display = "block";
+  if (consent === "accepted") loadGoogleAnalytics();
+
+  document.getElementById("cookie-accept").onclick = function () {
+    localStorage.setItem("cookieConsent", "accepted");
+    loadGoogleAnalytics();
+    banner.style.display = "none";
+  };
+
+  document.getElementById("cookie-decline").onclick = function () {
+    localStorage.setItem("cookieConsent", "declined");
+    banner.style.display = "none";
+  };
+});
+</script>
